c3d9ca0391d578fafc3a13b43bc2a1b33d2b706f
Daniel Erwan <contact@erwandaniel.fr>
2026-01-10 01:17:54 +0100

    feat: Add ColorModeContext to enable light/dark theme switching in admin app

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -8,7 +8,7 @@ import routerProvider, {
 import dataProvider from '@refinedev/simple-rest'
 import {BrowserRouter, Route, Routes, Outlet} from 'react-router'
 import {ConfigProvider, App as AntdApp} from 'antd'
-import { PartitionOutlined, UsbOutlined, NodeExpandOutlined } from '@ant-design/icons'
+import {PartitionOutlined, UsbOutlined, NodeExpandOutlined} from '@ant-design/icons'
 import {useNotificationProvider, ThemedLayout, ErrorComponent} from '@refinedev/antd'
 import '@refinedev/antd/dist/reset.css'
 import DeviceList from "@/pages/devices/list.tsx";
@@ -19,6 +19,7 @@ import MixerList from "@/pages/mixers/list.tsx";
 import MixerCreate from "@/pages/mixers/create.tsx";
 import MixerEdit from "@/pages/mixers/edit.tsx";
 import {Dashboard} from "@/pages/dashboard";
+import {ColorModeContextProvider} from "./contexts/color-mode";
 
 const API_URL = import.meta.env.VITE_API_URL || '/api'
 
@@ -26,83 +27,89 @@ function App() {
     return (
         <BrowserRouter basename={import.meta.env.BASE_URL}>
             <RefineKbarProvider>
-                <ConfigProvider>
-                    <AntdApp>
-                        <Refine
-                            dataProvider={dataProvider(API_URL)}
-                            notificationProvider={useNotificationProvider}
-                            routerProvider={routerProvider}
-                            resources={[
-                                {
-                                    name: 'dashboard',
-                                    list: '/dashboard'
-                                },
-                                {
-                                    name: 'devices',
-                                    list: '/devices',
-                                    create: '/devices/create',
-                                    meta: {
-                                        icon: <UsbOutlined />
-                                    }
-                                },
-                                {
-                                    name: 'camilladsp/pipelines',
-                                    list: '/camilladsp/pipelines',
-                                    create: '/camilladsp/pipelines/create',
-                                    meta: {
-                                        label: 'Pipelines',
-                                        icon: <PartitionOutlined />
+                <ColorModeContextProvider>
+                    <ConfigProvider>
+                        <AntdApp>
+                            <Refine
+                                dataProvider={dataProvider(API_URL)}
+                                notificationProvider={useNotificationProvider}
+                                routerProvider={routerProvider}
+                                resources={[
+                                    {
+                                        name: 'dashboard',
+                                        list: '/dashboard'
                                     },
-                                },
-                                {
-                                    name: 'camilladsp/mixers',
-                                    list: '/camilladsp/mixers',
-                                    create: '/camilladsp/mixers/create',
-                                    edit: '/camilladsp/mixers/edit/:id',
-                                    meta: {
-                                        label: 'Mixers',
-                                        icon: <NodeExpandOutlined />
+                                    {
+                                        name: 'devices',
+                                        list: '/devices',
+                                        create: '/devices/create',
+                                        meta: {
+                                            icon: <UsbOutlined/>
+                                        }
                                     },
-                                },
-                            ]}
-                            options={{
-                                syncWithLocation: true,
-                                warnWhenUnsavedChanges: true,
-                                title: {text: 'open-cinema', icon: <img src={`${import.meta.env.BASE_URL}logo.svg`} alt="logo" style={{ width: 24 }} />}
-                            }}
-                        >
-                            <Routes>
-                                <Route
-                                    element={
-                                        <ThemedLayout>
-                                            <Outlet/>
-                                        </ThemedLayout>
+                                    {
+                                        name: 'camilladsp/pipelines',
+                                        list: '/camilladsp/pipelines',
+                                        create: '/camilladsp/pipelines/create',
+                                        meta: {
+                                            label: 'Pipelines',
+                                            icon: <PartitionOutlined/>
+                                        },
+                                    },
+                                    {
+                                        name: 'camilladsp/mixers',
+                                        list: '/camilladsp/mixers',
+                                        create: '/camilladsp/mixers/create',
+                                        edit: '/camilladsp/mixers/edit/:id',
+                                        meta: {
+                                            label: 'Mixers',
+                                            icon: <NodeExpandOutlined/>
+                                        },
+                                    },
+                                ]}
+                                options={{
+                                    syncWithLocation: true,
+                                    warnWhenUnsavedChanges: true,
+                                    title: {
+                                        text: 'open-cinema',
+                                        icon: <img src={`${import.meta.env.BASE_URL}logo.svg`} alt="logo"
+                                                   style={{width: 24}}/>
                                     }
-                                >
-                                    <Route index element={<NavigateToResource resource="devices"/>}/>
-                                    <Route path="/dashboard" element={<Dashboard />} />
-                                    <Route path="/devices" >
-                                        <Route index element={<DeviceList/>}/>
-                                        <Route path="create" element={<DeviceCreate/>}/>
+                                }}
+                            >
+                                <Routes>
+                                    <Route
+                                        element={
+                                            <ThemedLayout>
+                                                <Outlet/>
+                                            </ThemedLayout>
+                                        }
+                                    >
+                                        <Route index element={<NavigateToResource resource="devices"/>}/>
+                                        <Route path="/dashboard" element={<Dashboard/>}/>
+                                        <Route path="/devices">
+                                            <Route index element={<DeviceList/>}/>
+                                            <Route path="create" element={<DeviceCreate/>}/>
+                                        </Route>
+                                        <Route path="/camilladsp/pipelines">
+                                            <Route index element={<PipelineList/>}/>
+                                            <Route path="create" element={<PipelineCreate/>}/>
+                                        </Route>
+                                        <Route path="/camilladsp/mixers">
+                                            <Route index element={<MixerList/>}/>
+                                            <Route path="create" element={<MixerCreate/>}/>
+                                            <Route path="edit/:id" element={<MixerEdit/>}/>
+                                        </Route>
+                                        <Route path="*" element={<ErrorComponent/>}/>
                                     </Route>
-                                    <Route path="/camilladsp/pipelines">
-                                        <Route index element={<PipelineList/>}/>
-                                        <Route path="create" element={<PipelineCreate/>}/>
-                                    </Route>
-                                    <Route path="/camilladsp/mixers">
-                                        <Route index element={<MixerList/>}/>
-                                        <Route path="create" element={<MixerCreate/>}/>
-                                        <Route path="edit/:id" element={<MixerEdit/>}/>
-                                    </Route>
-                                    <Route path="*" element={<ErrorComponent/>}/>
-                                </Route>
-                            </Routes>
-                            <RefineKbar/>
-                            <UnsavedChangesNotifier/>
-                            <DocumentTitleHandler/>
-                        </Refine>
-                    </AntdApp>
-                </ConfigProvider>
+                                </Routes>
+                                <RefineKbar/>
+                                <UnsavedChangesNotifier/>
+                                <DocumentTitleHandler/>
+                            </Refine>
+                        </AntdApp>
+                    </ConfigProvider>
+                </ColorModeContextProvider>
             </RefineKbarProvider>
         </BrowserRouter>
     )
